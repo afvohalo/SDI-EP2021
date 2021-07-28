b465997c147671d19d29dffce4cc57930bab7d0f
felosc <49010129+felosc@users.noreply.github.com>
2021-07-28 14:15:46 -0500

    estylo con bootstraps
    icono y tabla

--- P_unicatolica/views/documento/table.php
+++ P_unicatolica/views/documento/table.php
@@ -1,20 +1,16 @@
-<div id="table_data">
+<div id="table_data ">
     <!-- Todo lo que este dentro de este id, se actualiza, si esta afuera o antes de esto, va a pasar un bucle -->
 
-    <div class="row">
-        <div class="col s1 m1 l1 xl1"> </div>
-        <div class="col s10 m10 l10 xl10">
-            <div class="card z-depth-5">
+    <div class="container text-center">
+        <div class="col s10 ">
                 <div>
-                    <div class="card-title center">
+                    <div class="">
                         <h4>
                             MODULO TIPO DOCUMENTO
                         </h4>
                     </div>
-                    <div class="card-content">
-                        <div class="col s2"></div>
-                        <div class="col s8">
-                            <table id="myTable" class="responsive-table centered striped">
+                    <div class="container table-responsive">                                                
+                            <table id="myTable" class="table table-bordered text-center">
                                 <thead>
                                     <tr>
                                         <th>Tipo Documento</th>
@@ -26,15 +22,18 @@
                                                                 
 foreach ($this->d as $value) {
     ?>
-
+                                    <div class="row">
+                                        <div class="col">                                        
                                     <tr>
                                         <td><?php echo $value['Tip_doc_nombre'] ?></td>
                                         <td>
-                                            <button data-target="modalDocumentoedit" class="btn modal-trigger waves-effect amber darken-4" onclick="OneData(<?php echo $value['Tip_doc_id'] ?>)"><i class="material-icons">edit</i></button>
+                                            <button data-target="modalDocumentoedit" class="btn modal-trigger" onclick="OneData(<?php echo $value['Tip_doc_id'] ?>)"><i class="material-icons">edit</i></button>
 
                                             <button onclick="DeleteData(<?php echo $value['Tip_doc_id'] ?>)" class="btn modal-trigger waves-effect red darken-4"><i class="material-icons">delete</i></button>
                                         </td>
                                     </tr>
+                                        </div>
+                                    </div>
                                 <?php
 }
 ?>
@@ -43,12 +42,12 @@ foreach ($this->d as $value) {
                         </div>
                     </div>
 
-                    <div class="card-action center">
-                        <button data-target="modalDocumento" class="btn modal-trigger waves-effect buttonsApp"><i class="material-icons left">library_add</i>Agregar Nuevo Tipo De Documento</button>
+                    <div class="">
+                        <button data-target="modalDocumento" class="btn modal-trigger waves-effect ">
+                            <i class="material-icons left">library_add</i><br>Agregar Nuevo Tipo De Documento</button>
                     </div>
                 </div>
             </div>
         </div>
+        </div>
     </div>
-    <div class="col s1 m1 l1 xl1"> </div>
-</div>
