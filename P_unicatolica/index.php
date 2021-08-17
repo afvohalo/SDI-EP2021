b36731770868f10253976e342068ab317590e764
isa98-08 <79162516+isa98-08@users.noreply.github.com>
2021-08-17 13:00:29 -0500

    prueba

--- P_unicatolica/index.php
+++ P_unicatolica/index.php
@@ -5,4 +5,4 @@ require_once 'libs/view.php';
 require_once 'libs/model.php';
 require_once 'config/config.php';
 require_once 'libs/app.php'; // Todo el aplicativo
-/* $app = new App(); */aaa
+/* $app = new App(); */
