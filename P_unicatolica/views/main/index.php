14bbcf8580e3b34a8e598576d0519a73ed1845dc
isa98-08 <79162516+isa98-08@users.noreply.github.com>
2021-09-27 14:56:37 -0500

    ultimo cambio estructura 27/09/2021

--- P_unicatolica/views/main/index.php
+++ P_unicatolica/views/main/index.php
@@ -314,8 +314,7 @@
 
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-
+      
     </div>
 
     
@@ -332,9 +331,3 @@
         z-index: 1;
     }
 </style>
-<script>
-    $('.carousel').carousel({
-        interval: 2000,
-
-    });
-</script>
