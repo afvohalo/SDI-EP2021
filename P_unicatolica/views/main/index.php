f9bd3bd6bfdbac86c123058757e0f1242a8fca9c
isa98-08 <79162516+isa98-08@users.noreply.github.com>
2021-10-07 15:54:54 -0500

    algunos cambios footer

--- P_unicatolica/views/main/index.php
+++ P_unicatolica/views/main/index.php
@@ -280,20 +280,6 @@
                     </form>
                 </div>
             </div>
-            <footer class="bg-ligth my-5 pt-5 text-muted text-center text-small">
-                <div class="py-3 text-center">
-                    <img class="d-block mx-auto mb-4" src="public/img/logo.svg" alt="" width="300" height="100">
-                    <p class="lead">Institución de Educación Superior sujeta a inspección y vigilancia por el Ministerio
-                        de Educación Nacional – Resolución No. 944 de 1996 MEN – SNIES 2731 <br>
-
-                        Sede Principal Cra. 122 No. 12-459 Pance, Cali – Colombia
-                        Teléfonos: +57 (2) 555 2767 – +57 (2) 312 0038 <br>
-
-                        Para notificaciones judiciales y administrativas comuníquese a:
-                        secretariageneral@unicatolica.edu.co y juridico@unicatolica.edu.co</p>
-                </div>
-            </footer>
-
         </div>
 
 
