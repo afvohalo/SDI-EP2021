4372bfd69c017dfdbe66a1919e28682f178e898b
isaac <iarojas22@misena.edu.co>
2021-10-13 16:48:33 -0500

    cambios tablas temporal

--- P_unicatolica/views/tipodocumento/table.php
+++ P_unicatolica/views/tipodocumento/table.php
@@ -1,8 +1,8 @@
     <div id="table_data">
-    <!-- Todo lo que este dentro de este id, se actualiza, si esta afuera o antes de esto, va a pasar un bucle -->
+        <!-- Todo lo que este dentro de este id, se actualiza, si esta afuera o antes de esto, va a pasar un bucle -->
 
-    <div class="container text-center">
-        <div class="col s10 ">
+        <div class="container text-center">
+            <div class="col s10 ">
                 <div>
                     <div class="">
                         <h4>
@@ -10,45 +10,44 @@
                         </h4>
                     </div>
                     <div class="container table-responsive">
-                            <table id="myTable" class="table table-bordered text-center">
-                                <thead>
-                                    <tr>
-                                        <th>Tipo de documento</th>
-                                        <th class="notexportar">Acciones</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
+                        <table id="myTable" class="table table-bordered text-center">
+                            <thead>
+                                <tr class="bg-info">
+                                    <th>Tipo de documento</th>
+                                    <th class="notexportar">Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
                                 <?php
 
-foreach ($this->d as $value) {
-    ?>
+                                foreach ($this->d as $value) {
+                                ?>
                                     <div class="row">
                                         <div class="col">
-                                    <tr>
-                                        <td><?php echo $value['Tip_doc_nombre'] ?></td>
-                                        <td>
-                                            <button data-target="#modalTipodocumentoedit" class="btn btn-warning" data-toggle="modal" onclick="OneData(<?php echo $value['Tip_doc_id'] ?>)"><i  class="material-icons">edit</i></button>
+                                            <tr>
+                                                <td><?php echo $value['Tip_doc_nombre'] ?></td>
+                                                <td>
+                                                    <button data-target="#modalTipodocumentoedit" class="btn btn-warning" data-toggle="modal" onclick="OneData(<?php echo $value['Tip_doc_id'] ?>)"><i class="material-icons">edit</i></button>
 
-                                            <button onclick="DeleteData(<?php echo $value['Tip_doc_id'] ?>)" class="btn btn-danger"><i class="material-icons">delete</i></button>
-                                        </td>
-                                    </tr>
+                                                    <button onclick="DeleteData(<?php echo $value['Tip_doc_id'] ?>)" class="btn btn-danger"><i class="material-icons">delete</i></button>
+                                                </td>
+                                            </tr>
                                         </div>
                                     </div>
                                 <?php
-}
-?>
-                                </tbody>
-                            </table>
-                        </div>
+                                }
+                                ?>
+                            </tbody>
+                        </table>
                     </div>
-                    <div class="">
-                        <button  data-target="#modalTipoDocumento" class="btn btn-primary" data-toggle="modal">
-                            <i class="material-icons left">library_add</i><br>Agregar Nuevo Tipo de Documento</button>
-                    </div>
-
                 </div>
+                <div class="">
+                    <button data-target="#modalTipoDocumento" class="btn btn-primary" data-toggle="modal">
+                        <i class="material-icons left">library_add</i><br>Agregar Nuevo Tipo de Documento</button>
+                </div>
+
             </div>
         </div>
-        </div>
     </div>
-
+    </div>
+    </div>
