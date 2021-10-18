1f22b9b132e86ba812ed179c5fdc8a397dece2e2
felosc <49010129+felosc@users.noreply.github.com>
2021-10-17 20:08:02 -0500

    creacion archivos modulo salas usuario

---
+++ P_unicatolica/views/salasusuario/index.php
@@ -0,0 +1,12 @@
+<script src="public/js/scripts_salasusuario.js"></script>
+
+<div id="modalSalasUsuarioedit" class="modal">
+    <div class="modal-content">
+        <h4 class="center">Editar</h4>
+
+            <div id="form_edit">
+
+            </div>
+
+    </div>
+</div>
