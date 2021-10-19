bf9abab1f94033764281ab695be6821e693c5d21
felosc <49010129+felosc@users.noreply.github.com>
2021-10-18 20:53:53 -0500

    actulizacion archivo js salausuario

---
+++ P_unicatolica/public/js/scripts_salasusuario.js
@@ -0,0 +1,51 @@
+function SaveData() {
+	Validate_Form(()=>{
+		Message_Confirm(()=>{
+			var data = new FormData($("#form_salasUsuario")[0]);	
+			var DatosSalasUsuario ={
+				"Ctr":"Salasusuario",
+				"Acc":"registrarSalasUsuario",
+				"formularioSalasUsuario":ProcessData(data)
+			}
+				console.log(DatosSalasUsuario);
+			ReadyChangeJquery(DatosSalasUsuario,"#table_data");
+		})
+	},"#form_salasUsuario")
+}
+
+function OneData(id){
+	var OneIdSalasUsuario ={
+		'Ctr':'SalasUsuario',
+		'Acc':'OneSalasUsuario',
+		'id':id
+	}
+	console.log(OneIdSalasUsuario);
+	ReadyChangeJquery(OneIdSalasUsuario,"#form_edit")
+}	
+
+
+function DeleteData(id){
+	Message_Confirm(()=>{
+		var SalasUsuarioDelete ={
+			"Ctr":"SalasUsuario",
+			"Acc":"deleteSalasUsuario",
+			"id":id,
+		}
+		ReadyChangeJquery(SalasUsuarioDelete,"#table_data")
+	})
+}
+
+function UpdateData(id){
+	Validate_Form(()=>{
+		Message_Confirm(()=>{
+		var data = new FormData($('#form_salasUsuario_edit')[0]);
+		var DatosSalasUsuarioEdit ={
+			'Ctr':'SalasUsuario',
+			'Acc':'editSalasUsuario',
+			'id':id,
+			'formularioSalasUsuarioEdit':ProcessData(data)
+		}
+		ReadyChangeJquery(DatosSalasUsuarioEdit,'#table_data')
+	})
+},'#form_salasUsuario_edit')
+}
