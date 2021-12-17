92dd54bda67c8b2e358d25dd095093aaf4d62556
felosc <49010129+felosc@users.noreply.github.com>
2021-12-16 21:55:30 -0500

    se agregan equipos a la lista y se arregla error para mostrar datos en litar con  gualal

--- P_unicatolica/views/tipodocumento/table.php
+++ P_unicatolica/views/tipodocumento/table.php
@@ -8,6 +8,11 @@
                         <h4>
                             MODULO TIPO DE DOCUMENTO
                         </h4>
+                        <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+
                     </div>
                     <div class="container table-responsive">
                         <table id="myTable" class="table table-bordered text-center">
