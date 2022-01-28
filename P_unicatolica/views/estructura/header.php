c25c2c889c6966a1ab43bd4f99dbeb20f4154559
felosc <49010129+felosc@users.noreply.github.com>
2022-01-28 10:05:15 -0500

    se soluciona problema del menu de tablas pequeñas acmodando los src en el archivo head.php

--- P_unicatolica/views/estructura/header.php
+++ P_unicatolica/views/estructura/header.php
@@ -3,9 +3,9 @@
 
 <head>
     <script src="public/js/jquery-3.4.1.slim.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="public/js/bootstrap.js"></script>
     <script src="public/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
     </script>
